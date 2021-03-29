8a343be1f5570489bae5274cc23cd9b22a818fd8
Santi <scorredoira@gmail.com>
2021-03-29 18:17:16 +0200

    better console.log object serialization

--- lib/console.go
+++ lib/console.go
@@ -23,27 +23,32 @@ var Console = []dune.NativeFunction{
 		Function: func(this dune.Value, args []dune.Value, vm *dune.VM) (dune.Value, error) {
 			var lastInline bool
 			for i, v := range args {
+				if i > 0 {
+					fmt.Fprint(vm.GetStdout(), " ")
+				}
 				var s string
 				switch v.Type {
 				case dune.String, dune.Int, dune.Float, dune.Bool:
-					if i > 0 {
-						fmt.Fprint(vm.GetStdout(), " ")
-					}
 					s = v.String()
 					fmt.Fprint(vm.GetStdout(), s)
 					lastInline = true
 				default:
-					ojb := v.Export(0)
-					str, ok := ojb.(fmt.Stringer)
-					if ok {
-						fmt.Fprintln(vm.GetStdout(), str.String())
-					} else {
-						b, err := json.MarshalIndent(ojb, "", "    ")
-						if err != nil {
+					obj := v.ExportMarshal(0)
+					if m, ok := obj.(json.Marshaler); ok {
+						if err := marshal(m, vm); err != nil {
 							return dune.NullValue, err
 						}
-						s = string(b)
-						fmt.Fprintln(vm.GetStdout(), s)
+						continue
+					}
+
+					str, ok := obj.(fmt.Stringer)
+					if ok {
+						fmt.Fprintln(vm.GetStdout(), str.String())
+						continue
+					}
+
+					if err := marshal(obj, vm); err != nil {
+						return dune.NullValue, err
 					}
 				}
 			}
@@ -55,4 +60,13 @@ var Console = []dune.NativeFunction{
 			return dune.NullValue, nil
 		},
 	},
+}
+
+func marshal(obj interface{}, vm *dune.VM) error {
+	b, err := json.MarshalIndent(obj, "", "    ")
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(vm.GetStdout(), string(b))
+	return nil
 }
