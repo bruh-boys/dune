7f976892bd0feae6815b65fe885d88e6dd785e5e
Santi <scorredoira@gmail.com>
2021-05-13 08:14:17 +0200

    windows build: enable CGO for sqlite

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 go build -o bin/dune cmd/dune/*.go
 GOOS=darwin GOARCH=amd64 go build -o bin/dune-mac cmd/dune/*.go
-GOOS=windows GOARCH=amd64 go build -o bin/dune.exe cmd/dune/*.go
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -o bin/dune.exe cmd/dune/*.go
 
 # sudo apt-get install mingw-w64
 # GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -o bin/dune.exe dune/main.go
