c866cf49966961535fdb43d84e341f9a590a167c
Santi <scorredoira@gmail.com>
2021-07-23 17:07:26 +0200

    console.debug

--- lib/console.go
+++ lib/console.go
@@ -13,55 +13,91 @@ func init() {
 
 declare namespace console {
 	export function log(...v: any[]): void
+
+	export function enableDebug(): void
+	export function disableDebug(): void
+	export function debug(...v: any[]): void
 }
 `)
 }
+
+var debugLogEnabled bool
 
 var Console = []dune.NativeFunction{
 	{
 		Name:      "console.log",
 		Arguments: -1,
 		Function: func(this dune.Value, args []dune.Value, vm *dune.VM) (dune.Value, error) {
-			var lastInline bool
-			for i, v := range args {
-				if i > 0 {
-					fmt.Fprint(vm.GetStdout(), " ")
-				}
-				var s string
-				switch v.Type {
-				case dune.String, dune.Int, dune.Float, dune.Bool:
-					s = v.String()
-					fmt.Fprint(vm.GetStdout(), s)
-					lastInline = true
-				default:
-					obj := v.ExportMarshal(0)
-
-					str, ok := obj.(fmt.Stringer)
-					if ok {
-						fmt.Fprintln(vm.GetStdout(), str.String())
-						continue
-					}
-
-					if m, ok := obj.(json.Marshaler); ok {
-						if err := marshal(m, vm); err != nil {
-							return dune.NullValue, err
-						}
-						continue
-					}
-
-					if err := marshal(obj, vm); err != nil {
-						return dune.NullValue, err
-					}
-				}
-			}
-
-			if lastInline {
-				fmt.Fprintln(vm.GetStdout())
-			}
-
+			return consoleLog(args, vm)
+		},
+	},
+	{
+		Name:      "console.enableDebug",
+		Arguments: -1,
+		Function: func(this dune.Value, args []dune.Value, vm *dune.VM) (dune.Value, error) {
+			debugLogEnabled = true
 			return dune.NullValue, nil
 		},
 	},
+	{
+		Name:      "console.disableDebug",
+		Arguments: -1,
+		Function: func(this dune.Value, args []dune.Value, vm *dune.VM) (dune.Value, error) {
+			debugLogEnabled = false
+			return consoleLog(args, vm)
+		},
+	},
+	{
+		Name:      "console.debug",
+		Arguments: -1,
+		Function: func(this dune.Value, args []dune.Value, vm *dune.VM) (dune.Value, error) {
+			if !debugLogEnabled {
+				return dune.NullValue, nil
+			}
+			return consoleLog(args, vm)
+		},
+	},
+}
+
+func consoleLog(args []dune.Value, vm *dune.VM) (dune.Value, error) {
+	var lastInline bool
+	for i, v := range args {
+		if i > 0 {
+			fmt.Fprint(vm.GetStdout(), " ")
+		}
+		var s string
+		switch v.Type {
+		case dune.String, dune.Int, dune.Float, dune.Bool:
+			s = v.String()
+			fmt.Fprint(vm.GetStdout(), s)
+			lastInline = true
+		default:
+			obj := v.ExportMarshal(0)
+
+			str, ok := obj.(fmt.Stringer)
+			if ok {
+				fmt.Fprintln(vm.GetStdout(), str.String())
+				continue
+			}
+
+			if m, ok := obj.(json.Marshaler); ok {
+				if err := marshal(m, vm); err != nil {
+					return dune.NullValue, err
+				}
+				continue
+			}
+
+			if err := marshal(obj, vm); err != nil {
+				return dune.NullValue, err
+			}
+		}
+	}
+
+	if lastInline {
+		fmt.Fprintln(vm.GetStdout())
+	}
+
+	return dune.NullValue, nil
 }
 
 func marshal(obj interface{}, vm *dune.VM) error {
