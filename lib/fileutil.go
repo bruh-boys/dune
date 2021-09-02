f048d2fedc7ea395b75e1d1bb18cb4bd0570180f
Santi <scorredoira@gmail.com>
2021-09-02 12:53:53 +0200

    fileutil.isDirEmpty

--- lib/fileutil.go
+++ lib/fileutil.go
@@ -12,12 +12,49 @@ func init() {
 
 declare namespace fileutil { 
     export function copy(src: string, dst: string): byte[]
+
+	export function isDirEmpty(path: string, fs?: io.FileSystem): boolean
 }
 
 `)
 }
 
 var FileUtil = []dune.NativeFunction{
+	{
+		Name:      "fileutil.isDirEmpty",
+		Arguments: -1,
+		Function: func(this dune.Value, args []dune.Value, vm *dune.VM) (dune.Value, error) {
+			if err := ValidateOptionalArgs(args, dune.String, dune.Object); err != nil {
+				return dune.NullValue, err
+			}
+			if err := ValidateArgRange(args, 1, 2); err != nil {
+				return dune.NullValue, err
+			}
+
+			fs, ok := args[1].ToObject().(*FileSystemObj)
+			if !ok {
+				return dune.NullValue, fmt.Errorf("invalid filesystem argument, got %v", args[1])
+			}
+
+			name := args[0].String()
+
+			f, err := fs.FS.Open(name)
+			if err != nil {
+				return dune.NullValue, err
+			}
+			defer f.Close()
+
+			_, err = f.Readdir(1)
+			if err != nil {
+				if err == io.EOF {
+					return dune.NewBool(true), nil
+				}
+				return dune.NullValue, err
+			}
+
+			return dune.NewBool(false), nil
+		},
+	},
 	{
 		Name:      "fileutil.copy",
 		Arguments: 2,
